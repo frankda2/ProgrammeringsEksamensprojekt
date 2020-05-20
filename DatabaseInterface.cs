99e2393c09d9f69b768589b34c3d8097fe8de270
Studsgaard <48673440+Studsgaard@users.noreply.github.com>
2020-05-20 17:05:07 +0200

    Created basic classes

---
+++ DatabaseInterface.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammeringsEksamensprojekt
+{
+	class DatabaseInterface
+	{
+		//The connection string for connecting to the database
+		private string connString = "Server=(local);" + //Server name here ex. (local) for local hosted DB
+									"Database=whm_data;" +  //Database name, DO NOT CHANGE
+									"Trusted_Connection=true";
+
+		//Method for reading an item from the database, using the item number
+		public Item GetItem(int itemNo) {
+			//Opening a connection to the database
+			using(SqlConnection conn = new SqlConnection(connString))
+			{
+				Item newItem; //For storing the created "item"
+
+				//Creating the commmand
+				SqlCommand command = new SqlCommand("", conn);
+
+				conn.Open();
+
+				
+				
+
+				//Returning the item read from the database
+				return newItem;
+			}
+		}
+	}
+}
---
+++ Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammeringsEksamensprojekt
+{
+	class Item
+	{
+		int id;         //The id of the item in the database
+		int itemNo;		//The item number
+		string name;
+		int stock;
+		int locationId; //The id of the items location in the database
+
+		//Constructor method, for creating an item from the database
+		public Item() {
+
+		}
+	}
+}
