d06275dfbc94f96e52aad7c60a3cf3bbc70948a8
Studsgaard <48673440+Studsgaard@users.noreply.github.com>
2020-06-06 16:10:23 +0200

    Revert "Merge branch 'master' of https://github.com/frankda2/ProgrammeringsEksamensprojekt"
    This reverts commit aae10b9ab3d89080ff7824da5359855e7d675d99, reversing
    changes made to 4a1f46b0ebc69fa52ca04c66d3976aac3975b256.

--- PageElement.cs
+++ PageElement.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProgrammeringsEksamensprojekt
 {
-    class PageElement
+    class PageElement //"Child class" til specifikke side elementer
     {
         public PageElement(int function, int width, int height, int startX, int startY, string text)
         {
@@ -24,7 +24,7 @@ namespace ProgrammeringsEksamensprojekt
         string stringInputToDatabase;
         int intInputToDatabase;
 
-        readonly private int function;
+        readonly private int function; //remove readonly?
         public int Width { get; set; } //Make private get or set
         public int Height { get; set; }
         public int StartX { get; set; }
@@ -40,7 +40,7 @@ namespace ProgrammeringsEksamensprojekt
                     Console.CursorVisible = true;
                     this.stringInputToDatabase = Console.ReadLine();
                     Console.CursorVisible = false;
-                    Page.Menu(Page.ProductRegistrationPage);
+                    Page.Menu(Page.ProductRegistrationPage); //remove?
                     break;
                 case 2:
                     //Numeric input field
@@ -77,11 +77,14 @@ namespace ProgrammeringsEksamensprojekt
                     tempInt = Page.ProductRegistrationPage.pageElementList[3].intInputToDatabase;
                     tempString = Page.ProductRegistrationPage.pageElementList[4].stringInputToDatabase;
 
-                    for (int i = 1; i < 5; i++)
-                    {
-                        Console.SetCursorPosition(Page.ProductRegistrationPage.pageElementList[i].StartX + 1, Page.ProductRegistrationPage.pageElementList[i].StartY + 2);
-                        Console.WriteLine(new string(' ', 56)); //Should prob not be a fixed value
-                    }
+                    Console.SetCursorPosition(Page.ProductRegistrationPage.pageElementList[1].StartX + 1, Page.ProductRegistrationPage.pageElementList[1].StartY + 2);
+                    Console.WriteLine(new string(' ', 56)); //Should not be a fixed value
+                    Console.SetCursorPosition(Page.ProductRegistrationPage.pageElementList[2].StartX + 1, Page.ProductRegistrationPage.pageElementList[2].StartY + 2);
+                    Console.WriteLine(new string(' ', 56)); //Should not be a fixed value
+                    Console.SetCursorPosition(Page.ProductRegistrationPage.pageElementList[3].StartX + 1, Page.ProductRegistrationPage.pageElementList[3].StartY + 2);
+                    Console.WriteLine(new string(' ', 56)); //Should not be a fixed value
+                    Console.SetCursorPosition(Page.ProductRegistrationPage.pageElementList[4].StartX + 1, Page.ProductRegistrationPage.pageElementList[4].StartY + 2);
+                    Console.WriteLine(new string(' ', 56)); //Should not be a fixed value
                     Page.Menu(Page.ProductRegistrationPage);
                     break;
 
